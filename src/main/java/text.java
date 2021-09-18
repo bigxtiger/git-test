74053cf97243cd49d0c84ca45a93394fe5d1429d
xy <yangzi@guigu.com>
2021-09-19 00:26:11 +0800

    secord

---
+++ src/main/java/text.java
@@ -0,0 +1,10 @@
+/**
+ * @author xy
+ * @create 2021-09-19 0:22
+ */
+public class text {
+    public static void main(String[] args) {
+        System.out.println("sss");
+        System.out.println("bbbb");
+    }
+}
